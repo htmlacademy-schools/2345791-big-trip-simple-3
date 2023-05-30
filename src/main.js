f5866db572dff7ac0d9eb2d5c09119dd1ff70dd3
Damir <kim2.da@edu.spbstu.ru>
2023-05-31 01:50:14 +0300

    c

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import BoardPresenter from './presenter/boardPresenter';
 import PointsModel from './model/pointModel';
 
 const siteMainElement = document.querySelector('.page-body__page-main');
+const tripControls = document.querySelector('.trip-controls__filters');
 const siteHeaderElement = siteMainElement.querySelector('.trip-events');
 
 const pointsModel = new PointsModel();
 const boardPresenter = new BoardPresenter();
 
 render(new NewPointButtonView(), siteHeaderElement);
-render(new FilterView(), siteMainElement);
+render(new FilterView(), tripControls);
 
-boardPresenter.init(siteMainElement, pointsModel);
+boardPresenter.init(siteHeaderElement, pointsModel);
