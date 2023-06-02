9c992db43eab3a7f0f11b871e08667bac7c2d9bf
Damir <kim2.da@edu.spbstu.ru>
2023-06-02 22:05:56 +0300

    wrong branch

---
+++ src/presenter/pointPresenter.js
@@ -0,0 +1,75 @@
+import {render, replace, remove} from '../framework/render.js';
+import PointView from '../view/pointView.js';
+import PointEditView from '../view/editPointView.js';
+
+export default class PointPresenter {
+  #pointListContainer = null;
+
+  #pointComponent = null;
+  #pointEditComponent = null;
+
+  #point = null;
+
+  constructor(pointListContainer) {
+    this.#pointListContainer = pointListContainer;
+  }
+
+  init = (point) => {
+    this.#point = point;
+
+    const prevPointComponent = this.#pointComponent;
+    const prevPointEditComponent = this.#pointEditComponent;
+
+    this.#pointComponent = new PointView(point);
+    this.#pointEditComponent = new PointEditView(point);
+
+    this.#pointComponent.setEditClickHandler(this.#handleEditClick);
+    this.#pointEditComponent.setFormSubmitHandler(this.#handleFormSubmit);
+
+    if (prevPointComponent === null || prevPointEditComponent === null) {
+      render(this.#pointComponent, this.#pointListContainer);
+      return;
+    }
+
+    if (this.#pointListContainer.contains(prevPointComponent.element)) {
+      replace(this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#pointListContainer.contains(prevPointEditComponent.element)) {
+      replace(this.#pointEditComponent, prevPointEditComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevPointEditComponent);
+  };
+
+  delete = () => {
+    remove(this.#pointComponent);
+    remove(this.#pointEditComponent);
+  };
+
+  #replaceCardToForm = () => {
+    replace(this.#pointEditComponent, this.#pointComponent);
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+  };
+
+  #replaceFormToCard = () => {
+    replace(this.#pointComponent, this.#pointEditComponent);
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+  };
+
+  #escKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.#replaceFormToCard();
+    }
+  };
+
+  #handleEditClick = () => {
+    this.#replaceCardToForm();
+  };
+
+  #handleFormSubmit = () => {
+    this.#replaceFormToCard();
+  };
+}
